e2561963a964453b36782cb9d2dabb0d5436878e
Rabiul hossain <mohammadrabiul4@gmail.com>
2023-03-24 10:43:14 -0700

    find, findById, findByUser, updateById, delete from db, find winners all the functions are implemented in db.

--- db/db.js
+++ db/db.js
@@ -33,28 +33,89 @@ class MyDB {
         return result;
     }
 
-    // return all tickets
+    // return all the available tickets
     find() {
+        return this.tickets
+    } 
+
+    /**
+     * find ticket
+     * @param {string} ticketId
+     * @returns {Ticket}
+     *  * */ 
+    findById(ticketId) {
+        const ticket = this.tickets.find(ticket => {
+            /**
+             * @param {Ticket} ticket
+             */
+            ticket.id === ticketId;
+        });
+        return ticket;
 
     }
 
-    // find ticket
-    findById() {
-
+    /**
+     * find all tickets for a given user
+     * @param {string} username
+     * @returns {Array<Ticket>}
+     */
+    findByUser(username){
+        const tickets = this.tickets.filter(ticket => ticket.username === username);
+        return tickets;
     }
 
-    // update ticket
-    updateById() {
+    /**
+     * update ticket
+     * @param {string} ticketId
+     * @param{{username: string, price: number}} ticketBody
+     * @returns {Ticket}
+     */
+    updateById(ticketId, ticketBody) {
+        const ticket = this.findById(ticketId);
+        ticket.username = ticketBody.username ?? ticket.username;
+        ticket.price = ticketBody.price ?? ticket.price;
+        ticket.upadatedAt = new Date();
 
+        return ticket;
     }
 
-    // delete ticket
-    deleteById() {
-
+    /**
+     * delete ticket from db
+     * @param {string} ticketId
+     */
+    deleteById(ticketId) {
+        const index = this.tickets.findIndex(
+            /**
+            * @param {string} 
+            */
+           (ticket) => ticket.id === ticketId
+        );
+        if(index !== -1){
+            this.tickets.splice(index, 1);
+            return true;
+        } else {
+            return false;
+        }
+        
     }
+    
+    /**
+     * find winners
+     * @param {number} winnerCount
+     * @returns {Array<Ticket>}
+     */
+    draw(winnerCount) {
+        let indexes = new Array(winnerCount);
+        for(i = 0; i < indexes.length; i++){
+            const index = Math.floor(Math.random() * this.tickets.length);
+            while(indexes.includes(index)){
+                index = Math.floor(Math.random() * this.tickets.length);
+            }
+            indexes.push(index);
+        }
 
-    draw() {
-
+        const winners = indexes.map(index => this.tickets[index]);
+        return winners;
     }
 
 }
